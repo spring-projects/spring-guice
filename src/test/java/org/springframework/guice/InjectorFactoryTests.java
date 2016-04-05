097a86e4a2005206aaa0701657afda65a1730265
Taylor Wicksell <twicksell@netflix.com>
2016-04-05 10:52:20 -0700

    add test case for InjectorFactory usage added startup exception when multiple InjectorFactory definitions are provided

---
+++ src/test/java/org/springframework/guice/InjectorFactoryTests.java
@@ -0,0 +1,50 @@
+package org.springframework.guice;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.context.ApplicationContextException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.guice.annotation.ModuleRegistryConfiguration;
+import org.springframework.guice.injector.InjectorFactory;
+
+import com.google.inject.Guice;
+
+public class InjectorFactoryTests  {
+    
+    static final private InjectorFactory injectorFactory = Mockito.mock(InjectorFactory.class);
+    
+    @Before
+    public void init() {
+        Mockito.when(injectorFactory.createInjector(Mockito.anyList())).thenReturn(Guice.createInjector());
+    }
+    
+    @Test
+    public void testCustomInjectorIsCreated() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(InjectorFactoryConfig.class, ModuleRegistryConfiguration.class);
+        Mockito.verify(injectorFactory, Mockito.times(1)).createInjector(Mockito.anyList());
+    }
+    
+    @Test(expected=ApplicationContextException.class)
+    public void testMultipleInjectorFactoriesThrowsApplicationContextException() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(InjectorFactoryConfig.class, SecondInjectorFactoryConfig.class, ModuleRegistryConfiguration.class); 
+    }
+    
+    @Configuration
+    static class InjectorFactoryConfig {
+        @Bean
+        public InjectorFactory injectorFactory() { 
+           return injectorFactory;
+        }
+    }
+    
+    @Configuration
+    static class SecondInjectorFactoryConfig {
+        @Bean
+        public InjectorFactory injectorFactory2() { 
+            return injectorFactory;
+        }
+    }
+}
